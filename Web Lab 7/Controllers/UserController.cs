9d109008833216234766a983da701cbaea7e5e70
Zohaib Shahid <bsef21m008@pucit.edu.pk>
2024-05-02 19:07:04 +0500

    Lab 6 and 7 Solution

---
+++ Web Lab 7/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab7.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
